3066616ce7d693af0a82a06a41d231d4f8716e5d
Haze <85334552+Haze-ts@users.noreply.github.com>
2022-07-02 18:50:54 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,13 +2,18 @@ Config = {}
 
 Config.EnableCommand = false -- True if you want to enable the command
 
+Config.EnabeBlip = true -- True if you want to enable a blip map
+
 Config.Command = 'bulletmenu'
 
 Config.Price = 500 -- This is a price of the Bulletproof
 
 Config.PositionShop = { -- These are the locations of the shops
     vector3(18.397445678711,-1110.1365966797,29.797027587891),
-    vector3(-1308.5015869141,-390.99105834961,36.695774078369)
+    vector3(-1308.5015869141,-390.99105834961,36.695774078369),
+    vector3(812.67144775391,-2153.6303710938,29.61901473999),
+    vector3(-329.48861694336,6079.3608398438,31.454778671265),
+    vector3(2570.3586425781,297.91317749023,108.73486328125)
 }
 
 Lang = {
@@ -21,5 +26,7 @@ Lang = {
     ['bulletproof']                             = 'Giubbotto Antiproiettile',
     --Notify
     ['the_bulletproof_vest_is_worn']            = 'Il giubbotto è usurato',
-    ['you_do_not_have_enough_money']            = 'Non hai abbastanza soldi'
+    ['you_do_not_have_enough_money']            = 'Non hai abbastanza soldi',
+    --Blip Map
+    ['blip_map']                                = 'Negozio Giubbotti'
 }
